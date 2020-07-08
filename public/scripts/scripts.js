91fd9d8ba72658414cdd97d7203c1141811b7e83
ADRIANO BARRETO MONTEIRO DE ALMEIDA <adriano.malmeida-ext@viavarejo.com.br>
2020-07-08 20:27:38 -0300

    Adicionando o desafio 03 - Refatorando Foodfy

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,34 +1,63 @@
-const modalOverlay = document.querySelector('.modal-overlay')
 
 const recipes = document.querySelectorAll('.recipe')
 
-const modal = document.querySelector('.modal')
+// Solução usando o 'i' para incremento: 
+for (let i = 0; i < recipes.length; i++) {
+    recipes[i].addEventListener('click', () => {
+        window.location.href = `/recipe/${ i }`;
+    });
+}
 
-function verifyMaximize() {
-    if (modal.classList.contains('maximize')){
-        modal.classList.remove('maximize')
-    } else {
-        modal.classList.add('maximize')
+
+// Funções de exibir ou esconder as informações da receita
+function showHide(id) {
+    var display = document.getElementById(id).style.display;
+    var element = ''
+    if (id == "ingredients"){
+        element = document.getElementById("ingredients-button");
     }
+    else if (id == "preparation") {
+        element = document.getElementById("preparation-button");
+    }
+    else {
+        element = document.getElementById("information-button");
+    }
+    
+    
+    if(display == "none") {
+        document.getElementById(id).style.display = 'block';
+        element.innerHTML="ESCONDER";
+        // console.log('mostrou...')
+    } else {
+        document.getElementById(id).style.display = 'none';
+        // console.log('escondeu...')
+        element.innerHTML="MOSTRAR";
+    }
+
 }
 
-for (let recipe of recipes) {
-    recipe.addEventListener('click', function () {
-        const recipe_id = recipe.getAttribute('id')
-        modalOverlay.classList.add('active')
-        verifyMaximize()
-        modalOverlay.querySelector('img').src = `../../public/asserts/${recipe_id}.png`
-        modalOverlay.querySelector('img').alt = recipe_id
+//  Backup para analise e estudo posteriormente
+// Solução usando o id, gerado no HTML pelo indexOf:
+// for (let recipe of recipes) {
+//     recipe.addEventListener('click', () => {
+//         const recipe_id = recipe.getAttribute('id')
+//         // console.log(recipe_id)
+//         window.location.href = `/recipe/${recipe_id}`;
+//     }
+// }
 
-        modalOverlay.querySelector('.descricao-receita').innerHTML = recipe.querySelector('.recipe__content').innerHTML
-        modalOverlay.querySelector('.criador-receita').innerHTML = recipe.querySelector('.recipe__info').innerHTML
+// for (let recipe of recipes.length) {
+//     recipe.addEventListener('click', function () {
+//         const recipe_index = recipes.indexOf(recipe)
+//         console.log(recipe_index)
+//         window.location.href = `/recipe/${recipe_index}`
+        
+//         // const recipe_id = recipe.getAttribute('id')
+//         // modalOverlay.querySelector('img').src = `/asserts/${recipe_id}.png`
+//         // modalOverlay.querySelector('img').alt = recipe_id
 
-    })
-}
+//         // modalOverlay.querySelector('.descricao-receita').innerHTML = recipe.querySelector('.recipe__content').innerHTML
+//         // modalOverlay.querySelector('.criador-receita').innerHTML = recipe.querySelector('.recipe__info').innerHTML
 
-
-document.querySelector('.close-modal').addEventListener('click', function () {
-    modalOverlay.classList.remove('active')
-    verifyMaximize()
-})
-
+//     })
+// }
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+const recipes = require('./data')
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("src/views", {
+    express:server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function(req, res) {
+    return res.render("home", { items: recipes })
+})
+
+server.get("/about", function(req, res) {
+    return res.render("about")
+})
+
+
+server.get("/recipes", function(req, res) {
+    return res.render("recipes", { items: recipes })
+})
+
+server.get("/recipe/:index", function(req, res) {
+    const receitas = recipes
+    
+    const recipeIndex =  req.params.index
+
+    // console.log(recipeIndex)
+    // console.log(receitas[recipeIndex])
+
+    const recipe = recipes.find(function(recipe) {
+        // console.log(recipe[recipeIndex])
+        return recipe == receitas[recipeIndex]
+    })
+
+    if (!recipe){
+        return res.send("Recipe not found!")
+    }
+
+    return res.render("recipe", { item: recipe })
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+})
+
+
+
+server.listen(5000, function() {
+    console.log("server is running...")
+})
